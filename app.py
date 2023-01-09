4f739aa15781d12c821f84d476053518ed124208
Ashwani Kumar Tiwari <tiwariashwani3012@gmail.com>
2023-01-09 14:01:11 +0530

    add flask framework and requirement file

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
-def text_to_hex():
-  text = input('Enter the string: ')
-  # Encode the text into a bytes object
-  b = text.encode('utf-8')
+from flask import Flask
+app = Flask(__name__)
 
-  # Convert the bytes object into a list of integers
-  int_list = list(b)
-
-  # Convert the list of integers into a list of hexadecimal strings
-  hex_list = [hex(i) for i in int_list]
-
-  # Concatenate the hexadecimal strings into a single string
-  hex_string = ''.join(hex_list)
-
-  print(hex_string)
-
+@app.route("/")
+def home():
+    return "Hello, this is a sample Python Web App running on Flask Framework!!!"
 
 if __name__ == "__main__":
-    text_to_hex()
+    app.run()
